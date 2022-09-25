91f0474cdc21bec9f461019bde64148d0e8ac528
yagishuhei <shuhei.y3166@gmail.com>
2022-09-25 14:59:18 +0900

    database,insert

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Tweet;
 class IndexController extends Controller
 {
     /**
@@ -15,7 +15,9 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('tweet.index', ['name' => 'laravel']);    
+        $tweets = Tweet::all();
+        return view('tweet.index')
+            ->with('tweets',$tweets);    
     }
     
 }
